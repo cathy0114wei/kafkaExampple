bb8f45b112a86f51956b985419b8f2734830b394
Cathy wei <cathywei@CathydeMacBook-Pro.local>
2022-08-06 19:38:57 -0700

    kafka and wiki

--- kafka-producer-w/src/main/java/cathy/springboot/WikimediaChangesProducer.java
+++ kafka-producer-w/src/main/java/cathy/springboot/WikimediaChangesProducer.java
@@ -1,10 +1,16 @@
 package cathy.springboot;
 
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
 
 //import javax.annotation.processing.SupportedAnnotationTypes;
 
@@ -19,9 +25,15 @@ public class WikimediaChangesProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(){
+    public void sendMessage() throws InterruptedException {
         String topic = "wikimedia_recentchanges";
 
+        EventHandler eventHandler = new WikimediaChangesHandler(kafkaTemplate, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+        eventSource.start();
 
+        TimeUnit.MINUTES.sleep(10);
     }
 }
